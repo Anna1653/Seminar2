ab9aa7eb059fc5c03e4f8d4a294b0bb564859dca
Anna <anuta1653@mail.ru>
2023-06-19 14:40:10 +0300

    ConsoleApp3(1)

---
+++ ConsoleApp3(1)/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+
+Console.WriteLine("Введите день недели в числовом формате: ");
+int number = int.Parse(Console.ReadLine());
+
+string day = null;
+switch (number)
+{
+    case (1):
+        day = "Понедельник";
+        break;
+    case (2):
+        day = "Вторник";
+        break;
+    case (3):
+        day = "Среда";
+        break;
+    case (4):
+        day = "Четверг";
+        break;
+    case (5):
+        day = "Пятница";
+        break;
+    case (6):
+        day = "Суббота";
+        break;
+    case (7):
+        day = "Воскресенье";
+        break;
+    default:
+        Console.WriteLine("Такого дня недели не существует)))");
+        break;
+}
+if (day != null)
+{
+    if ( number > 6)
+    {
+        Console.WriteLine($"{day} - выходной день");
+    }   
+    else
+    {
+        Console.WriteLine($"{day} - будний день");
+    }
+}
